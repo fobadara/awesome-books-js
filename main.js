22505e455f4ee1399dbcef4b2282d7cbdbd20df1
fobadara <fobadara@gmail.com>
2021-10-20 08:32:33 +0100

    Add class to project

--- main.js
+++ main.js
@@ -1,28 +1,39 @@
 const container = document.querySelector('.container');
 let books = [];
+
 container.innerHTML = `
-<div class="books"></div>
-<form>
-    <input type="text" class="title" placeholder="Title" required><br><br>
-    <input type="text" class="author" placeholder="Author" required class><br><br>
-    <button class="add">Add</button>
-    </form>
-`;
-function NewBooks(title, author) {
-  this.title = title;
-  this.author = author;
+  <div class="books"></div>
+  <form>
+      <input type="text" class="title" placeholder="Title" required><br><br>
+      <input type="text" class="author" placeholder="Author" required class><br><br>
+      <button class="add">Add</button>
+      </form>
+  `;
+
+  const add = container.querySelector('.add'); 
+  const title = container.querySelector('.title').value; 
+  const author = container.querySelector('.author').value; 
+
+class Library {
+  addBooks = (title, author) => {
+    const book = {title, author};
+    console.log(book);
+    books.push(book); 
+  }
+
+  // addBooks = (title, author) => {
+  //   this.book = new NewBks(title, author); 
+  // }
+
+  editLocalStorage = (index) => {
+    if(localStorage.books) {
+      books = JSON.parse(localStorage.books);
+      const data = books.filter((book) => book !== books[index]); 
+      localStorage.setItem('books', JSON.stringify(data));  
+    }
+  }
 }
 
-const add = container.querySelector('.add'); 
-const title = container.querySelector('.title'); 
-const author = container.querySelector('.author'); 
-function addBooks(title, author) { 
-  const book = new NewBooks(title, author); 
-  books.push(book); 
-}
-
-function editLocalStorage(index) {
-  books = JSON.parse(localStorage.books);
-  const data = books.filter((book) => book !== books[index]); 
-  localStorage.setItem('books', JSON.stringify(data));
-}
+const library = new Library();
+// library.addBooks();
+// library.editLocalStorage();
